0b62335e66190cdb56d53d67eb4bec35db1dc60b
tgunaydin <tgunaydin@gmail.com>
2025-11-14 13:22:01 +0300

    First commit

---
+++ FinallyExplorer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FinallyExplorer
+//
+//  Created by temel gunaydin on 14.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FinallyExplorer/FinallyExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinallyExplorerApp.swift
+//  FinallyExplorer
+//
+//  Created by temel gunaydin on 14.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct FinallyExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
